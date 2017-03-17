fb8428b87037c149b5aa58d95d04e00e7dc3449c
VIPIRAK-LAPTOP\Andreas <Andreas.l.gade@gmail.com>
2017-03-17 10:45:34 +0100

    go Update

---
+++ AwesomeThreadingFun/AwesomeThreadingFun/Components/Component.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeThreadingFun.Components
+{
+    class Component
+    {
+        public GameObject Gameobject;
+
+        public Component(GameObject go)
+        {
+            this.Gameobject = go;
+        }
+    }
+}
--- AwesomeThreadingFun/AwesomeThreadingFun/GameObject.cs
+++ AwesomeThreadingFun/AwesomeThreadingFun/GameObject.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Graphics;
 using System.Threading;
+using AwesomeThreadingFun.Components;
 
 namespace AwesomeThreadingFun
 {
@@ -13,15 +14,15 @@ namespace AwesomeThreadingFun
         private bool kill;
         private Thread UpdateThread;
 
+        private List<Component> components;
+
         public GameObject()
         {
             this.kill = false;
         }
 
         public void Draw(SpriteBatch sb)
-        {
-
-        }
+            => components.FindAll(c => c is IDrawable).ForEach(c => (c as IDrawable).Draw(sb));
 
         private void Update()
         {
@@ -39,5 +40,17 @@ namespace AwesomeThreadingFun
 
         public void Kill()
             => this.kill = true;
+
+        public T GetComponent<T>() where T : Component
+            => components.Find(c => c is T) as T;
+
+        public Component GetComponent(Func<Component, bool> Filter)
+            => components.Find(c => Filter(c));
+
+        public T[] GetComponents<T>() where T : Component
+            => (from c in components where c is T select c as T).ToArray();
+
+        public Component[] GetComponents(Func<Component, bool> Filter)
+            => components.FindAll(c => Filter(c)).ToArray();
     }
 }
