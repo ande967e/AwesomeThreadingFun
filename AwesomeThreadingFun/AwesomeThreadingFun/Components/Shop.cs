aeef5269990b6b7ff0c3e1dd61f072ec724f8d4c
Simon <theboysen@hotmail.com>
2017-03-20 13:30:09 +0100

    shop

--- AwesomeThreadingFun/AwesomeThreadingFun/Components/Shop.cs
+++ AwesomeThreadingFun/AwesomeThreadingFun/Components/Shop.cs
@@ -5,25 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using AwesomeThreadingFun.ShopItems;
 
 
 namespace AwesomeThreadingFun.Components
 {
-    class Shop : Component, IUpdateable
+    class Shop : Component, IUpdateable, IInteractable
     {
         private int Stock;
         private int Money;
-        new List<Loadingbay> contracts;
+        new List<Loadingbay> loadingbays;
+
         
 
         public Shop (GameObject go) : base (go)
         {
-
+            loadingbays = new List<Loadingbay>();
+            loadingbays.Add(new Loadingbay());
         }
         
-        private void Update(TimeSpan time)
+        public void Update(TimeSpan time)
         {
             
+        }
+
+        public Loadingbay RequestLoadingBay()
+        {
+            lock{ return loadingbays.Find(l => l.)}
         }
 
         private void UpdateStock()
