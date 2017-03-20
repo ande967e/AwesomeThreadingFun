fab94e6d4da2ceb1d0265583c64f1c7835b0c9c9
Anders <mufas.anders@hotmail.com>
2017-03-20 12:35:03 +0100

    Factory - WIP

--- AwesomeThreadingFun/AwesomeThreadingFun/Components/Factory.cs
+++ AwesomeThreadingFun/AwesomeThreadingFun/Components/Factory.cs
@@ -4,17 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Graphics;
+using AwesomeThreadingFun.Builder;
 
 namespace AwesomeThreadingFun.Components
 {
-    class Factory : Component, IUpdateable, IDrawable
+    class Factory : Component, IUpdateable
     {
-        private int spawnSpeed; //the time between trucks are send on it's way.
         private int truckCargoSize; //the amount of cargo the truck carries.
         private int truckTravelSpeed; //the speed of which the truck travels.
         private int truckOnloadTime; //Time it takes to onload a truck.
-        private int contractTime; //the amount of time the contract holds before it stops.
         private int contractAmount; //amount of contracts.
+        private Director director;
+        private int contractTime; //the amount of time the contract holds before it stops.          --> Miliseconds.
+        private int contractTimer; //indicates the amount of time the contract have been active.
+        private int spawnSpeed; //the time between trucks are send on it's way.                     --> Miliseconds.
+        private int spawnTimer; //indicates the upcounter until next truck spawn.
 
         public Factory(GameObject go, int spawnSpeed, int truckCargoSize, int truckTravelSpeed, int contractTime, int truckOnloadTime) : base(go)
         {
@@ -23,26 +27,41 @@ namespace AwesomeThreadingFun.Components
             this.truckTravelSpeed = truckTravelSpeed;
             this.truckOnloadTime = truckOnloadTime;
             this.contractTime = contractTime;
-        }
-
-        public void Draw(SpriteBatch sb)
-        {
+            director = new Director(new Truckbuilder(this.Gameobject, this.truckTravelSpeed, this.truckCargoSize, this.truckOnloadTime));
 
         }
 
         public void Update(TimeSpan time)
         {
+            //runs if there are any contracts.
+            if (contractAmount > 0)
+            {
+                //removes a contract if it's time limit is reached.
+                contractTimer += time.Milliseconds;
+                if (contractTimer >= contractTime)
+                {
+                    contractAmount--;
+                    contractTimer = 0;
+                }
 
+                //adds a truck if enough time has passed.
+                spawnTimer += time.Milliseconds;
+                if (spawnTimer > spawnSpeed)
+                {
+                    SpawnTruck();
+                    spawnTimer = 0;
+                }
+            }  
         }
 
         public void SpawnTruck()
         {
-
+            director.BuildObject();
         }
 
         public void BuyContract()
         {
-            
+            contractAmount++;
         }
     }
 }
