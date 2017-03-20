e4527d32f3635377b24bc91a526100203c94cfea
Simon <theboysen@hotmail.com>
2017-03-20 11:53:31 +0100

    Shop klassen (eller noget <3)

---
+++ AwesomeThreadingFun/AwesomeThreadingFun/Components/Shop.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace AwesomeThreadingFun.Components
+{
+    class Shop : Component, IUpdateable
+    {
+        private int Stock;
+        private int Money;
+        new List<Contracts> contracts;
+
+        public Shop (GameObject go) : base (go)
+        {
+
+        }
+        
+        private void Update()
+        {
+            
+        }
+
+        private void UpdateStock()
+        {
+            if ()
+            {
+
+            }
+
+            if ()
+            {
+
+            }
+        }
+    }
+}
