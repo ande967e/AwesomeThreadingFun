c50e22b0049823894a552e432c9a7b1a256905c9
VIPIRAK-LAPTOP\Andreas <Andreas.l.gade@gmail.com>
2017-03-20 11:30:30 +0100

    ...

---
+++ AwesomeThreadingFun/AwesomeThreadingFun/Builder/Director.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeThreadingFun.Builder
+{
+    class Director
+    {
+        IBuilder builder;
+
+        public Director(IBuilder builder)
+        {
+            this.builder = builder;
+        }
+    }
+}
