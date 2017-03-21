e7bd70c16269f6ab74af0f944e94c0ccbd3b7cb8
Simon <theboysen@hotmail.com>
2017-03-21 12:06:02 +0100

    UI Klasse

---
+++ AwesomeThreadingFun/AwesomeThreadingFun/UI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace AwesomeThreadingFun
+{
+    class UI
+    {
+        private Texture2D UITexture;
+        private Rectangle UIRect;
+        private string assetName;
+
+        public UI(string assetName)
+        {
+            this.assetName = assetName;
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            UITexture = content.Load<Texture2D>(assetName);
+            UIRect = new Rectangle(0, 0, UITexture.Width, UITexture.Height);
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            spritebatch.Draw(UITexture, UIRect, Color.White);
+        }
+    }
+}
